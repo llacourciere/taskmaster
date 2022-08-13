1ae135d9cabc0eb4c9eccaf354ac4b4cbcded263
LaLacou <108777796+users.noreply.github.com>
2022-08-13 15:31:29 -0700

    added some personalized styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -195,6 +195,7 @@ var auditTask = function(taskEl) {
   else if(Math.abs(moment().diff(time, 'days'))<=2) {
     $(taskEl).addClass('list-group-item-warning');
   }
+  console.log(taskEl);
 };
 
 //allow tasks to be sorted or dragged to a different status
@@ -204,16 +205,18 @@ $('.card .list-group').sortable({
   tolerance: 'pointer',
   helper: 'clone',
   activate: function(event){
-    console.log('acitivate', this);
+    $(this).addClass('dropover');
+     $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function(event){
-    console.log('deactivate', this);
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   over: function(event){
-    console.log('over', event.target);
+    $(event.target).addClass('dropover-active');
   },
   out: function(event) {
-    console.log('out', event.target);
+    $(event.target).removeClass('dropover-active');
   },
   update: function(event) {
     var tempArr = [];
@@ -251,14 +254,19 @@ $("#trash").droppable ({
     console.log('drop');
   },
   over: function(event, ui) {
-    console.log('over');
+    $('.bottom-trash').addClass('bottom-trash-active')
   },
   out: function(event, ui) {
-    console.log('out');
-  }
-})
+    $('.bottom-trash').removeClass('bottom-trash-active')
+  } 
+});
 
 // load tasks for the first time
 loadTasks();
 
 
+setInterval(function() {
+  $('.card .list-group-item').each(function(index, el){
+    auditTask(el);
+  })
+}, (1000 *60)*30);
